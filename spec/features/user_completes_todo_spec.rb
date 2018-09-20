7939b997664306e855610d42f4850146cd36b735
kamalogudah <mcpaul2058@gmail.com>
2018-09-20 11:52:52 +0300

    Complete todos

--- spec/features/user_completes_todo_spec.rb
+++ spec/features/user_completes_todo_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-feature 'User Completes Todo' do
+feature 'User completes todo' do
   scenario 'successfully' do
     sign_in
+
     click_on 'Add a new todo'
     fill_in 'Title', with: 'Buy Milk'
     click_on 'Submit'
 
-    click_on 'Mark complete'
-    expect(page).to have_css '.todos li.compeleted', text: 'Buy Milk'
+    click_on 'Mark Complete'
 
+    expect(page).to have_css '.todos li.compeleted', text: 'Buy Milk'
   end
 end
