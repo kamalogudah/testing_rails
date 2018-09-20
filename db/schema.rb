e7a9222dedf6b9a9f4047f905285e03a3c04f06a
kamalogudah <mcpaul2058@gmail.com>
2018-09-20 11:24:27 +0300

    Allow Todo to be completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_16_194832) do
+ActiveRecord::Schema.define(version: 2018_09_20_081502) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.datetime "completed_at"
   end
 
 end
